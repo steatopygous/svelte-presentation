836f770fc1d38e62a776c6e751e73804a7315417
Harry Ohlsen <hohlsen@iinet.net.au>
2019-06-20 05:47:21 +1000

    WIP 2019-06-20 0545

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,11 +1,37 @@
 <script>
-    import ConditionalCode from './components/conditional-code.svelte';
-    import ConditionalStyles from './components/conditional-styles.svelte';
-    import Loops from './components/loops.svelte';
+	let promise = resolveAfterSnoozing(5);
+
+	async function resolveAfterSnoozing(seconds) {
+	    return new Promise((resolve, reject) => {
+    	    function getRandomNumber() {
+    	        console.log(`getRandomNumber()`);
+
+    	        const value = Math.random();
+
+                if (value > 0.5) {
+                    resolve(value);
+                } else {
+                    throw new Error('Random value too small');
+                }
+    	    }
+
+	        setTimeout(getRandomNumber, seconds * 1000);
+	    });
+	}
+
+	function handleClick() {
+		$: promise = getRandomNumber(5);
+	}
 </script>
 
-<ConditionalCode />
-<hr/>
-<Loops />
-<hr/>
-<ConditionalStyles />
+<button on:click={handleClick}>
+	generate random number
+</button>
+
+{#await promise}
+	<p>...waiting</p>
+{:then number}
+	<p>The number is {number}</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
