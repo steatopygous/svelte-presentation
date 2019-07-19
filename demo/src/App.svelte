37a6601baf796792c49641f91913345554df5350
Harry Ohlsen <harry.ohlsen@3plearning.com>
2019-07-19 13:36:33 +1000

    Initial version

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,17 +1,33 @@
 <script>
-    import Hello from './components/Hello.svelte';
-    import Goodbye from './components/Goodbye.svelte';
+    let framework  = 'React';
 
-	export let name;
+    $: src = `/Svelte.png`;
 </script>
 
 <style>
-	h1 {
-		color: purple;
-		font-size: 40px;
-	}
-</style>
-<h1>Switching Gears...</h1>
+    .container {
+        text-align: center;
+        height: 90vh;
+        width: auto;
+    }
 
-<Goodbye name="Virtual DOM" />
-<Hello {name} />
+    .Svelte {
+        /*width: 100%;*/
+        height: 100%;
+    }
+
+    .React {
+        width: 50%;
+        height: 100%;
+    }
+
+</style>
+
+<div class="container">
+    <input type="radio" bind:group={framework} value="React"> Non-Svelte
+    <input type="radio" bind:group={framework} value="Svelte"> Svelte
+
+    <p/>
+
+    <img {src} class={framework} alt="Steatopygous" />
+</div>
